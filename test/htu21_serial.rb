5a5b0d5aabfa94e81857dd0d442e8d7aca8ecb95
Hiroki Mori <yamori813@yahoo.co.jp>
2023-10-22 14:55:27 +0900

    add error check

--- test/htu21_serial.rb
+++ test/htu21_serial.rb
@@ -31,29 +31,34 @@ HTU21_READ_SERIAL_LAST_6BYTES_COMMAND = [0xFC, 0xC9]
 t = BsdIic.new(0)
 
 rcv_data = t.read(HTU21_ADDR, 8, HTU21_READ_SERIAL_FIRST_8BYTES_COMMAND)
-rcv_data += t.read(HTU21_ADDR, 6, HTU21_READ_SERIAL_LAST_6BYTES_COMMAND)
+if rcv_data == nil
+p "Read Error"
+else
+  rcv_data += t.read(HTU21_ADDR, 6, HTU21_READ_SERIAL_LAST_6BYTES_COMMAND)
 
-values = [0, 2, 4, 6]
-values.each do |n|
-  if !htu21_crc_check rcv_data[n], rcv_data[n+1]
-    raise
+  values = [0, 2, 4, 6]
+  values.each do |n|
+    if !htu21_crc_check rcv_data[n], rcv_data[n+1]
+      raise
+    end
   end
-end
 
-values = [8, 11]
-values.each do |n|
-  if !htu21_crc_check rcv_data[n] << 8 | rcv_data[n+1], rcv_data[n+2]
-    raise
+  values = [8, 11]
+  values.each do |n|
+    if !htu21_crc_check rcv_data[n] << 8 | rcv_data[n+1], rcv_data[n+2]
+      raise
+    end
   end
-end
 
-serial_number = ""
-values = [0, 2, 4, 6, 8, 9, 11, 12]
-values.each do |n|
-  serial_number += rcv_data[n].to_s(16).rjust(2, '0')
-end
+  serial_number = ""
+  values = [0, 2, 4, 6, 8, 9, 11, 12]
+  values.each do |n|
+    serial_number += rcv_data[n].to_s(16).rjust(2, '0')
+  end
 
-p serial_number
+  p serial_number
+
+end
 
 rescue
 p "CRC Error"
